8711910edd85d445f5de36ad153663d10e02bb17
thiago-cav <64841957+thiago-cav@users.noreply.github.com>
2022-02-27 15:50:44 +0200

    Update Cavalcante_etal_JZool_script.R

--- Cavalcante_etal_JZool_script.R
+++ Cavalcante_etal_JZool_script.R
@@ -1,6 +1,6 @@
 #Supplementary Material
 #Using similarity measures to identify non-random co-occurrences between intermittent behaviors: a case study with primate scent-marking
-#Thiago Cavalcante, Anamélia de Souza Jesus and Adrian A. Barnett
+#Thiago Cavalcante, AnamÃ©lia de Souza Jesus and Adrian A. Barnett
 ########### R script for bootstrapping procedure ###########
 
 
@@ -19,17 +19,17 @@ BiocManager::install("qvalue")
 behav1 <- sample(c(0,1), 100, replace=TRUE)
 behav2 <- sample(c(0,1), 100, replace=TRUE)
 
-jaccard.test.bootstrap(var1, var2, B=1000)#bootstrap with 1000 iterations 
+jaccard.test.bootstrap(behav1, behav2, B=1000)#bootstrap with 1000 iterations 
 
 ##### Simulations to identify the type I error rate #####
 
-#Example with N= 366 days
+#Example with N= 15 days
 #Note that you can use your own sample size (number of days) to test #the feasibility of this test for your study
 p = NA
 
 for(i in 1:100){
-  x1 <- sample(c(0,1), 366, replace=TRUE)
-  x2 <- sample(c(0,1), 366, replace=TRUE)
+  x1 <- sample(c(0,1), 15, replace=TRUE)
+  x2 <- sample(c(0,1), 15, replace=TRUE)
   test = jaccard.test.bootstrap(x1,x2,B=1000)
   p[i] = test$pvalue
 }
