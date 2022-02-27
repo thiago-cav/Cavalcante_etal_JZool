7ab8fd6002601432fd29a28f8631c499238747c2
thiago-cav <64841957+thiago-cav@users.noreply.github.com>
2022-02-27 14:33:24 +0200

    Add files via upload

---
+++ Cavalcante_etal_JZool_script.R
@@ -0,0 +1,39 @@
+#Supplementary Material
+#Using similarity measures to identify non-random co-occurrences between intermittent behaviors: a case study with primate scent-marking
+#Thiago Cavalcante, Anamélia de Souza Jesus and Adrian A. Barnett
+########### R script for bootstrapping procedure ###########
+
+
+#Installing and loading the packages
+install.packages("jaccard")
+library(jaccard)
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("qvalue")
+
+
+##### Creating two random behavioral vectors of 0x1 with a sample size of 
+#100 days #####
+behav1 <- sample(c(0,1), 100, replace=TRUE)
+behav2 <- sample(c(0,1), 100, replace=TRUE)
+
+jaccard.test.bootstrap(var1, var2, B=1000)#bootstrap with 1000 iterations 
+
+##### Simulations to identify the type I error rate #####
+
+#Example with N= 366 days
+#Note that you can use your own sample size (number of days) to test #the feasibility of this test for your study
+p = NA
+
+for(i in 1:100){
+  x1 <- sample(c(0,1), 366, replace=TRUE)
+  x2 <- sample(c(0,1), 366, replace=TRUE)
+  test = jaccard.test.bootstrap(x1,x2,B=1000)
+  p[i] = test$pvalue
+}
+
+mean(p < 0.05)
+table(p < 0.05)/100
+
